139ac7e3a091666fd1a6ddd90bcf0fdc00b379f3
AgathePons <ponsagathe37@gmail.com>
2022-07-25 15:13:26 +0200

    handleTaskInputChange done

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -7,21 +7,32 @@ import Form from '../Form';
 import Counter from '../Counter';
 import List from '../List';
 
-import tasks from '../../data/tasks';
+import taskData from '../../data/tasks';
 
 // == Composant
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      
+      tasks: taskData,
+      newTaskText: '',
     };
+
+    this.handleTaskInputChange = this.handleTaskInputChange.bind(this);
+  }
+
+  handleTaskInputChange(event) {
+    this.setState({ newTaskText: event.target.value });
   }
 
   render() {
+    const { newTaskText, tasks } = this.state;
     return (
       <div className="app">
-        <Form />
+        <Form
+          newTaskText={newTaskText}
+          onTaskInputChange={this.handleTaskInputChange}
+        />
         <Counter nbOfOngoingTasks={tasks.filter((t) => !t.done).length} />
         <List
           taskList={tasks}
--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -3,21 +3,23 @@ import PropTypes from 'prop-types';
 import './styles.scss';
 
 // == Composant
-function Form() {
+function Form({ newTaskText, onTaskInputChange }) {
   return (
     <form className="form">
       <input
         type="text"
         className="form-item"
         placeholder="Ajouter une tâche"
-        value=""
+        value={newTaskText}
+        onChange={onTaskInputChange}
       />
     </form>
   );
 }
 
 Form.propTypes = {
-
+  newTaskText: PropTypes.string.isRequired,
+  onTaskInputChange: PropTypes.func.isRequired,
 };
 
 // == Export
